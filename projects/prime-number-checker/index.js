0f89facf0400e97c6e2aa793d34587bff586ef4d
Bhavy Upreti <102848790+bhavyup@users.noreply.github.com>
2026-08-09 10:47:13 +0000

    feat: add prime number checker

---
+++ projects/prime-number-checker/index.js
@@ -0,0 +1,17 @@
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i * i <= number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = {
+  isPrime: isPrime
+};
